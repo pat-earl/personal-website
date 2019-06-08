c112e52f6d91b5015d66473a2966d87ed612cdac
Patrick Earl <patrick@pop-os.localdomain>
2019-06-07 20:23:29 -0400

    More work on chart system for viewing bandwidth

--- personal_website/views.py
+++ personal_website/views.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup as bs
 
 import pymongo
 import datetime
+import dateutil.parser
 
 # Personal Website stuff
 @app.route("/")
@@ -45,14 +46,9 @@ def youtube():
 
 
 # Bandwidth Monitor Project
-@app.route("/bandwidth")
+@app.route("/bandwidth/")
 def bandwidth():
-    try: 
-        client = pymongo.MongoClient(MONGO_DB_STRING)
-    except pymongo.errors.ConnectionFailure as e:
-        err_str = "PyMongo Error on Connection!<br>"
-        err_str += "Message: " + e.message()
-        return err_str
+    return render_template("bandwidth.html")
 
 # API Calls for it
 
@@ -97,13 +93,27 @@ def get_bandwidth(source, interface, interval, start, end):
         Path: ./api/v1/<source>/<interface>/<interval>/<start>/<end>/
         Description:
             Displays the number of megabytes sent in the given interval (seconds)
+
+        Parameters:
+            * Source - The MongoDB collection to query data from 
+            * Interface - The interface within the collection to gather data from
+            * Interval - The number of seconds to group the data into 
+            * Start - The start date for the data (Format: MMDDYYYY)
+            * End - The end date for the data (Format: MMDDYYYY)
     '''
 
     # If no date values are passed, do the last 30 days from today by default
     if start is None: 
-        start = datetime.datetime.now()
-        print(start)
-        end = start + datetime.timedelta(days=-30) 
+        end = datetime.datetime.now()
+        print("Default start datetime:", end)
+        start = end + datetime.timedelta(days=-2) 
+        print("Default end datetime:", start)
+
+    # Otherwise it is given, convert it to datetime
+    else:
+        start = datetime.datetime.strptime(start, "%m%d%Y")
+        end = datetime.datetime.strptime(end, "%m%d%Y")
+        print(start.isoformat())
         print(end)
 
     # Make sure valid arguments are passed
@@ -115,7 +125,7 @@ def get_bandwidth(source, interface, interval, start, end):
             raise IndexError("Invalid source")
         if interface not in interfaces:
             raise IndexError("Invalid interface")
-
+   
     except IndexError as e:
         abort(404, str(e))
 
@@ -123,21 +133,36 @@ def get_bandwidth(source, interface, interval, start, end):
     cursor = db[source].find({
         "interface": interface,
         "time": {
-            "$gte": end.isoformat(),
-            "$lt": start.isoformat()
+            "$gte": start.isoformat(),
+            "$lt": end.isoformat()
         }
-    })
+    }).sort([("time", 1)])
 
     return_dict = {
-        "interval": interval
+        "interval": interval,
+        "data": {}
     }
 
-    cur_interval = interval
+    cur_interval_time = None
+    cur_interval_time_str = ""
 
     for doc in cursor:
-        time = datetime.datetime.strptime(doc['time'], "%Y-%m-%dT%H:%M:%S%z")
-        cur_interval -= time.total_second
+        doc_time = dateutil.parser.parse(doc['time'])
+        
+        if cur_interval_time == None:
+            cur_interval_time = doc_time
+            cur_interval_time_str = cur_interval_time.strftime("%m-%d-%Y %H:%M:%S")
+            return_dict["data"][cur_interval_time_str] = {
+                "sent": 0,
+                "recv": 0
+            }
 
-        break
+        
+        if ((doc_time + datetime.timedelta(seconds=-interval)) 
+                > cur_interval_time):
+            cur_interval_time = None
+        
+        return_dict["data"][cur_interval_time_str]['sent'] += doc['send']
+        return_dict["data"][cur_interval_time_str]['recv'] += doc['recv']
     
-    return jsonify([test])
+    return jsonify(return_dict)
